dd8a15aa545794109c020985bd5173f60ba50500
Ian Nduta <gathonjiandutai@gmail.com>
2024-04-23 15:46:20 -0400

    Exercise 1

---
+++ tips.js
@@ -0,0 +1,11 @@
+var foodcost = 79.25;
+var tax = 6.54;
+var tip = 12.00;
+
+// calculations
+var totalDue = foodcost + tax + tip;
+
+//output 
+console.log("The total due is " + foodcost + " and tax is " + tax);
+console.log("Tip is " + tip.toFixed(2));
+console.log("Total Due is" + totalDue);
