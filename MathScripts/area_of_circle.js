5109fdad930247d5469e05ac3cccfa4ca12da82a
Ian Nduta <gathonjiandutai@gmail.com>
2024-04-23 19:49:15 -0400

    radius execise

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,11 @@
+"use script";
+
+var radius;
+var pi;
+
+radius = 20;
+pie = 3.14159;
+
+area = pie*(radius**2);
+
+console.log("The area of a circle with radius " + radius + " is " + area.toFixed(2));
