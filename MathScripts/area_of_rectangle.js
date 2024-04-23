ffabb7b3967dd24b11cad2bf5c3f3d6908861c7e
Ian Nduta <gathonjiandutai@gmail.com>
2024-04-23 17:54:01 -0400

    Area of a Rectangle exercise

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,15 @@
+"use strict";
+//Variables to find the area of a rectangle
+var width;
+var length;
+var area;
+
+//data
+
+width = 100;
+length = 200;
+
+//area
+area = width * length
+
+console.log("The area of the rectangle is " + area + "cm")
