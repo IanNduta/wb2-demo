af28a00975c5aac87ba345fb83287a54820fedec
Ian Nduta <gathonjiandutai@gmail.com>
2024-04-24 14:15:45 -0400

    else if time exercise

---
+++ IfScripts/greeting.js
@@ -0,0 +1,19 @@
+"use script";
+//varibles
+var goodM = "Good Morning!"
+var goodD = "Good Day!"
+var goodE = "Good Evening"
+var time = 25;
+
+if(time <= 10){
+    console.log(goodM);
+}
+else if(time <= 15){
+    console.log(goodD);
+}
+else if(time <= 23){
+    console.log(goodE);
+}
+else{
+    console.log("N/A");
+}
