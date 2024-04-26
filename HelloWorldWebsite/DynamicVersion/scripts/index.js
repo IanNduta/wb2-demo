dc0bc2186ee73bfd708d4fe517622798c8f46165
Ian Nduta <gathonjiandutai@gmail.com>
2024-04-26 14:13:43 -0400

    alert button

---
+++ HelloWorldWebsite/DynamicVersion/scripts/index.js
@@ -0,0 +1,16 @@
+"use scripts";
+console.log("index.js begin");
+window.onload = init;
+
+function init(){
+    const helloBtnElement = document.getElementById("helloBtn");
+    helloBtnElement.onclick = onHelloClicked;
+    console.log('index.js init() executed')
+}
+
+function onHelloClicked(){
+    alert('Hi there!');
+
+    console.log('index.js onHelloClicked() executed');
+}
+
