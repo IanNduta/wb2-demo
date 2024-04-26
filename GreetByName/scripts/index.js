8312d963e3386c3307dd2c9173e647140355b2ac
Ian Nduta <gathonjiandutai@gmail.com>
2024-04-26 15:15:29 -0400

    Created a button the will give a alert of the user name when clicked

---
+++ GreetByName/scripts/index.js
@@ -0,0 +1,17 @@
+"use strict";
+//loads index.js when the window is opened
+window.onload = init;
+
+//the init function
+function init(){
+    const greetBtn = document.getElementById("greetBtn");
+    greetBtn.onclick = onGreetUserBtnClicked;
+}
+function onGreetUserBtnClicked(){
+    const nameField = document.getElementById("nameField");
+    let name = nameField.value;
+    let message = "Hello " + name;
+    alert(message);
+    
+}
+
